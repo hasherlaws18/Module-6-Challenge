ad035dd7ec1b307eb8973169b235ca8b539d4813
hasherlaws18 <houstonasherlaws@gmail.com>
2023-01-24 22:55:42 -0600

    new changes ot script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,10 @@
-var searchbutton = $('.')
+
+var apikey= '55c9bf562e70a13fd086546beacbd343'
+var City= {city name}
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="
+
+//calling apis
+function GetWeather(){
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + City + '&appid=' + apikey;
+    fetch(queryURL)
+}
